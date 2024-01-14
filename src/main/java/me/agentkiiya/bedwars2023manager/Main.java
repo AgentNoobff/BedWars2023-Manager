8ff8982ce5fe1b3bb367cf4aaf108ca2902e7ce2
AgentNoobff <139374594+AgentNoobff@users.noreply.github.com>
2024-01-14 18:14:39 +0530

    Add files via upload

---
+++ src/main/java/me/agentkiiya/bedwars2023manager/Main.java
@@ -0,0 +1,17 @@
+package me.agentkiiya.bedwars2023manager;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
